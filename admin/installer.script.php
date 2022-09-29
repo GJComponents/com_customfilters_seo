56954de7904bc94277f009c768a2af3bc86e28d0
gartes <sad.net79@gmail.com>
2022-09-29 17:54:46 +0200

    version> add off filters & check err empty filter

--- admin/installer.script.php
+++ admin/installer.script.php
@@ -1,7 +1,17 @@
 <?php
-die(__FILE__ .' '. __LINE__ );
+
 
 class com_customfiltersInstallerScript
 {
+    /**
+     * Constructor
+     *
+     * @param   InstallerAdapter  $adapter  The object responsible for running this script
+     * @since 3.9
+     */
+    public function __construct(InstallerAdapter $adapter)
+    {
+        die(__FILE__ .' '. __LINE__ );
+    }
 
 }
