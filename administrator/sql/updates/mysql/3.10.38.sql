33addcf99c2fc0d7e2fced7b94396eb9ae430ea5
gartes <sad.net79@gmail.com>
2023-02-08 02:10:34 +0100

    version>3.10.38

--- administrator/sql/updates/mysql/3.10.38.sql
+++ administrator/sql/updates/mysql/3.10.38.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_filter_h1` TEXT NOT NULL AFTER `no_ajax`;
+ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_filter_h_tag` TEXT NOT NULL AFTER `no_ajax`;
 ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_filter_vm_cat_description` TEXT NOT NULL AFTER `sef_filter_h_tag`;
