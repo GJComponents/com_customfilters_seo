22d13216b87ca274351bbd8545c4d4f119973501
gartes <sad.net79@gmail.com>
2023-02-08 01:27:06 +0100

    version>3.10.37

--- administrator/sql/updates/mysql/3.10.37.sql
+++ administrator/sql/updates/mysql/3.10.37.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `gtvxq_cf_customfields_setting_seo` ADD `sef_filter_h1` TEXT NOT NULL AFTER `no_ajax`;
-ALTER TABLE `gtvxq_cf_customfields_setting_seo` ADD `sef_filter_vm_cat_description` TEXT NOT NULL AFTER `sef_filter_h_tag`;
+ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_filter_h1` TEXT NOT NULL AFTER `no_ajax`;
+ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_filter_vm_cat_description` TEXT NOT NULL AFTER `sef_filter_h_tag`;
