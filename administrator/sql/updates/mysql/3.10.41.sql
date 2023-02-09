15a41bcc11413fdd95232e084802a3f42340eb2e
gartes <sad.net79@gmail.com>
2023-02-09 17:10:37 +0100

    version>3.10.41

--- administrator/sql/updates/mysql/3.10.41.sql
+++ administrator/sql/updates/mysql/3.10.41.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE `#__cf_customfields_setting_seo`
+TRUNCATE TABLE `#__cf_customfields_setting_seo`;
 ALTER TABLE `#__cf_customfields_setting_seo` ADD `created_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `published`;
 ALTER TABLE `#__cf_customfields_setting_seo` ADD `sef_url_hash` VARCHAR(255) NOT NULL AFTER `sef_url`, ADD UNIQUE `sef_url_hash` (`sef_url_hash`(255));
 
